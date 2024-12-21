194590d39f286f14ebc6e9475644c3bf66157a8d
VBas <70689135+Bastiaan27@users.noreply.github.com>
2024-12-21 17:21:30 +0100

    Revert

--- src/main/kotlin/org/ptss/support/infrastructure/repositories/QuestionnaireBaseRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.ptss.support.infrastructure.repositories
-
-import jakarta.enterprise.context.ApplicationScoped
-import org.ptss.support.domain.models.Questionnaire
-
-@ApplicationScoped
-class QuestionnaireBaseRepository : BaseRepository<Questionnaire>()
