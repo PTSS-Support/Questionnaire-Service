9d871ee93abb4079770d427e8af4775db0671841
VBas <70689135+Bastiaan27@users.noreply.github.com>
2024-12-21 17:29:05 +0100

    wtf

--- src/main/kotlin/org/ptss/support/domain/interfaces/repositories/IQuestionnaireRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.ptss.support.domain.interfaces.repositories
-
-import org.ptss.support.domain.models.Questionnaire
-
-interface IQuestionnaireRepository {
-    fun getAll(): List<Questionnaire>
-}
